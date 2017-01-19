de5a1db3283bfe9e047d425e8858322cf39c20d6
Unsa Erum Sahato <Unsa@Unsas-MacBook-Air.local>
2017-01-18 23:08:48 -0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,7 @@ var socket = io();
 socket.on('connect', function(){
     console.log('Connected to the server')
 
-    socket.emit('createMessage',{
-        from: 'Fahar',
-        text: 'Yup, that works for me.'
-    })
+
 });
 
 socket.on('disconnect', function(){
