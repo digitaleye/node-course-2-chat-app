79170be0cff64f7cf3d15c011128c29e739ebc53
Unsa Erum Sahato <Unsa@Unsas-MacBook-Air.local>
2017-01-18 22:56:20 -0800

    Add neeMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function(){
+    console.log('Connected to the server')
+
+    socket.emit('createMessage',{
+        from: 'Fahar',
+        text: 'Yup, that works for me.'
+    })
+});
+
+socket.on('disconnect', function(){
+    console.log('Disconnected from the server')
+});
+
+socket.on('newMessage', function(message){
+    console.log('newMessage', message)
+})
